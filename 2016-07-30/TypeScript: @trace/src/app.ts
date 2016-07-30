79db8b59816a2266cb77d1abc3946b3e0a20af2b
Hasan Karahan <hasan.karahan@blackhan.com>
2016-07-30 16:13:39 +0400

    TypeScript Decorators: @trace -- w/overload

--- 2016-07-30/TypeScript: @trace/src/app.ts
+++ 2016-07-30/TypeScript: @trace/src/app.ts
@@ -4,32 +4,26 @@ import {trace, traceable} from './trace';
 @trace
 @named('App1')
 class App1 {
-
     public method1(a:number, b:string):void {
-        console.log('>', 'instance method');
     }
-
-    public static method2(a:number, b:string):void {
-        console.log('>', 'static method');
+    @traceable(false)
+    public method2(a:number, b:string):void {
     }
 }
 
 var app1 = new App1();
 app1.method1(1, 'text1');
-App1.method2(2, 'text2');
+app1.method2(2, 'text2');
 
 @named('App2')
 class App2 {
-    @traceable
     public method3(a:number, b:string):void {
-        console.log('>', 'instance method');
     }
     @traceable
-    public static method4(a:number, b:string):void {
-        console.log('>', 'static method');
+    public method4(a:number, b:string):void {
     }
 }
 
 var app2 = new App2();
 app2.method3(3, 'text3');
-App2.method4(4, 'text4');
+app2.method4(4, 'text4');
